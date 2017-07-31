172b1f373e81c2ccf471b33a1516421853f36953
Jared Stofflett <jared.stofflet@gmail.com>
2017-07-31 19:34:25 -0400

    Switch back to logging everything to the console. I will run this project in docker and console logging works with docker logs with out any changes.

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,27 +1,24 @@
-//grails-app/confg/logback.groovy
 import grails.util.BuildSettings
 import grails.util.Environment
-import ch.qos.logback.core.rolling.RollingFileAppender
-import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
-import ch.qos.logback.core.util.FileSize
+import org.springframework.boot.logging.logback.ColorConverter
+import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
-def HOME_DIR = "."
+import java.nio.charset.Charset
+
+conversionRule 'clr', ColorConverter
+conversionRule 'wex', WhitespaceThrowableProxyConverter
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
-    }
-}
+        charset = Charset.forName('UTF-8')
 
-appender("ROLLING", RollingFileAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
-    }
-    rollingPolicy(TimeBasedRollingPolicy) {
-        fileNamePattern = "${HOME_DIR}/logs/myApp-%d{yyyy-MM-dd_HH-mm}.log"
-        maxHistory = 30
-        totalSizeCap = FileSize.valueOf("2GB")
+        pattern =
+                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
     }
 }
 
@@ -34,11 +31,6 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
             pattern = "%level %logger - %msg%n"
         }
     }
-
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-    root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
 }
-else {
-    root(ERROR, ['ROLLING'])
-    logger 'com.jaredstofflett.grailstaskd', ERROR, ['ROLLING'],false
-}
+root(ERROR, ['STDOUT'])
